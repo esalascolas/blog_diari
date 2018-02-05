421803f32cb1c3d8d274bd75a08fa8d5263d33ce
Ernest Salas <esalas92@hotmail.com>
2018-02-05 22:03:55 +0100

    Modified settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -117,6 +117,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
